bb2891568e35a997f53ceedb626f9d8327dee2c3
Khalil Ltaief <92889256+xkhaliil@users.noreply.github.com>
2025-01-31 15:19:32 +0100

    Update NextAuth route handler to use imported configuration

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,1 +1,6 @@
-export { GET, POST } from "@/auth";
+import authConfig from "@/auth-config";
+import NextAuth from "next-auth";
+
+const handler = NextAuth(authConfig);
+
+export { handler as GET, handler as POST };
