b883bf39cb1744b07cad11cbf1ddc70875563a8a
Khalil Ltaief <92889256+xkhaliil@users.noreply.github.com>
2025-01-31 14:11:32 +0100

    Remove external auth configuration and reset NextAuth handler

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,9 @@
-import authConfig from "@/auth-config";
 import NextAuth from "next-auth";
+import Credentials from "next-auth/providers/credentials";
 
-const handler = NextAuth(authConfig);
-
-export { handler as GET, handler as POST };
+export const {
+  handlers: { GET, POST },
+  auth,
+} = NextAuth({
+  providers: [],
+});
